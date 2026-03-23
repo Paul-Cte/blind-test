4eb58c5cdcae0abde5ba1173d2b8067ffee62d67
excalurn <quentin.gentil38@gmail.com>
2026-03-23 18:34:08 +0100

    Load local storage + save à chaque ajout et favoris

--- model/playlist.js
+++ model/playlist.js
@@ -1,4 +1,5 @@
 import { Track } from "./track.js";
+import persist from "./persist.js";
 
 export class Playlist {
   constructor(data) {
@@ -9,7 +10,20 @@ export class Playlist {
     this.favorite = false;
   }
 
+  static loadStorage(data){
+    const playlist = Object.assign(new Playlist({ 
+        id: data.id, title: data.title, picture_big: data.cover,
+        tracks: { data: [] }
+    }), data);
+    playlist.tracks = data.tracks.map(t => Object.assign(new Track({ 
+        id: t.id, title: t.title, preview: t.preview,
+        artist: { name: t.artist }, album: { cover_big: t.cover }
+    }), t));
+    return playlist;
+  }
+
   changeFavorites(){
     this.favorite = !this.favorite;
+    persist.saveToLocalStorage();
   }
 }
